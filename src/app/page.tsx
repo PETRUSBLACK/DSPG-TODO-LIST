46023ec8e4219b45c1b00e0695fc2f66e886089e
Dev <onukwupetrusoge@gmail.com>
2025-06-24 16:40:31 +0100

    stocked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home(){
+const todos = prisma.todo.create({ data: { title: "test", complete: false}})
+
+export default async function Home(){
+
+  const todos = await prisma.todo.findMany()
+  console.log("hello")
+  console.log(todos)
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2x1">Todos</h1>
         <Link 
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"  href="/new">New</Link>
+          className="border border-slate-300 text-slate-300 px-2 py-1 
+          rounded hover:bg-slate-700 focus-within:bg-slate-700 
+          outline-none"  href="/new">New</Link>
       </header>
+
+    <ul className="pl-4">
+      {todos.map( (todo:any) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+
+
     </>
   )
 }
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,54 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client/extension";
+// // lib/prisma.ts
+// import { PrismaClient } from "./generated/prisma";
+// import { PrismaClient } from "@prisma/client/extension";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+// export const prisma =
+//   globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
+// import { PrismaClient } from "@prisma/client/extension";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma;
+// }
+
+
+
+// import { PrismaClient } from "./generated/prisma";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma;
+// }
+
+
+import { PrismaClient } from "./generated/prisma";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
